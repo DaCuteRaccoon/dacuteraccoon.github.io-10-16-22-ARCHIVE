e4b69955a16a2ed54b16c8e3820fa5f39cc63dc5
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-02-05 13:36:40 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+function Get(url){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",url,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
