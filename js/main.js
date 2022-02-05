a8196f50571eea7afc1788af5da18d0476c91d7a
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-02-05 13:39:40 -0800

    Add GET function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+// CREDIT FOR GET:
+// https://stackoverflow.com/questions/2499567/how-to-make-a-json-call-to-an-url
+
 function Get(url){
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",url,false);
     Httpreq.send(null);
     return Httpreq.responseText;          
 }
+
+var DaCuteRaccoon = JSON.parse(Get('https://api.github.com/users/DaCuteRaccoon'));
